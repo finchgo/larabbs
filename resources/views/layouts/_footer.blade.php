0ecaf4ef1c09439bfe436912bd3e86e784847101
vagrant <vagrant@homestead>
2021-10-19 05:53:43 +0000

    生成用户认证代码

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
         style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:749090670@qq.com">联系我们</a></p>
   </div>
 </footer>
