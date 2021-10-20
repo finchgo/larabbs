8bf762503de18b51eb278612fc0c7733fc968537
vagrant <vagrant@homestead>
2021-10-20 03:22:42 +0000

    找回密码成功消息提示

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
 
+Route::get('/', [PagesController::class, 'root'])->name('root');
 Auth::routes(['verify' => true]);
 
