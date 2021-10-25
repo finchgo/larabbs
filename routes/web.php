bf8223f74e1d4286ae67510f0699a62097d8bcd6
finchgo <since199703@outlook.com>
2021-10-25 06:25:43 +0000

    翻译URL标示

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,10 @@ Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', [TopicsController::class, 'uploadImage'])->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', [TopicsController::class, 'show'])->name('topics.show');
