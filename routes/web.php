11e9f20d7039b0d4a3cc213ba69cffb2158af408
finchgo <since199703@outlook.com>
2021-10-26 08:35:55 +0000

    消息通知列表

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post('upload_image', [TopicsController::class, 'uploadImage'])->name('top
 Route::get('topics/{topic}/{slug?}', [TopicsController::class, 'show'])->name('topics.show');
 
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
