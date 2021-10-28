43e3bb5391bc0cdb17fda8affce56d2b2b1213b9
finchgo <since199703@outlook.com>
2021-10-28 02:20:11 +0000

    站点权限部署

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,74 +1,54 @@
 @extends('layouts.app')
 
-@section('title', $topic->title)
-@section('description', $topic->excerpt)
+@section('title', $user->name . ' 的个人中心')
 
 @section('content')
 
   <div class="row">
 
-    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
       <div class="card ">
+        <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
         <div class="card-body">
-          <div class="text-center">
-            作者：{{ $topic->user->name }}
-          </div>
+          <h5><strong>个人简介</strong></h5>
+          <p>{{ $user->introduction }}</p>
           <hr>
-          <div class="media">
-            <div align="center">
-              <a href="{{ route('users.show', $topic->user->id) }}">
-                <img class="thumbnail img-fluid" src="{{ $topic->user->avatar }}" width="300px" height="300px">
-              </a>
-            </div>
-          </div>
+          <h5><strong>注册于</strong></h5>
+
+          <p>{{ $user->created_at->diffForHumans() }}</p>
         </div>
       </div>
     </div>
-
-    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-      <div class="card">
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+      <div class="card ">
         <div class="card-body">
-          <h1 class="text-center mt-3 mb-3">
-            {{ $topic->title }}
-          </h1>
-
-          <div class="article-meta text-center text-secondary">
-            {{ $topic->created_at->diffForHumans() }}
-            ⋅
-            <i class="far fa-comment"></i>
-            {{ $topic->reply_count }}
-          </div>
-
-          <div class="topic-body mt-4 mb-4">
-            {!! $topic->body !!}
-          </div>
-
-          @can('update', $topic)
-            <div class="operate">
-              <hr>
-              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-                <i class="far fa-edit"></i> 编辑
-              </a>
-              <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
-                    style="display: inline-block;"
-                    onsubmit="return confirm('您确定要删除吗？');">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit" class="btn btn-outline-secondary btn-sm">
-                  <i class="far fa-trash-alt"></i> 删除
-                </button>
-              </form>
-            </div>
-          @endcan
-
+          <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
         </div>
       </div>
+      <hr>
 
-      {{-- 用户回复列表 --}}
-      <div class="card topic-reply mt-4">
+      {{-- 用户发布的内容 --}}
+      <div class="card ">
         <div class="card-body">
-          @include('topics._reply_box', ['topic' => $topic])
-          @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}"
+                 href="{{ route('users.show', $user->id) }}">
+                Ta 的话题
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}"
+                 href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
+                Ta 的回复
+              </a>
+            </li>
+          </ul>
+          @if (if_query('tab', 'replies'))
+            @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+          @else
+            @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+          @endif
         </div>
       </div>
 
